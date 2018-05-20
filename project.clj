4237c69a85a3f74a82230f34952e7aeadc9a5c1e
Viktor Eikman <viktor.eikman@gmail.com>
2018-05-20 18:31:02 +0200

    v0.1.0 release. Updated project.clj.
    * Removed placeholder reference to the Apache license, which
      contradicted the readme and bundled licenses. Added an internally
      consistent AGPL reference instead.
    
    * Added a project URL.

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject dactyl-keyboard "0.1.0-SNAPSHOT"
+(defproject dactyl-keyboard "0.1.0"
   :description "A parametrized, split-hand, concave, columnar, erogonomic keyboard"
-  :url "http://example.com/FIXME"
+  :url "http://viktor.eikman.se/article/the-dmote/"
   :main dactyl-keyboard.dactyl
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "GNU Affero General Public License"
+            :url "https://www.gnu.org/licenses/agpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [unicode-math "0.2.0"]
                  [scad-clj "0.5.2"]])
