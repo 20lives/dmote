b7cc2d82f19d795b148d6b59c5977ee86c2d1a43
Viktor Eikman <viktor.eikman@gmail.com>
2018-12-08 07:24:16 +0100

    Updated dependencies.
    org.flatland/ordered v1.5.7 fixes compatibility with JDK11.

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "https://www.gnu.org/licenses/agpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/tools.cli "0.3.7"]
-                 [org.flatland/ordered "1.5.2"]
+                 [org.flatland/ordered "1.5.7"]
                  [clj-yaml "0.4.0"]
                  [scad-clj "0.5.3"]
                  [scad-tarmi "0.2.0-SNAPSHOT"]]
